8d421cfd9122b32a476df3d135fa3d4e1d95b9d7
Veniamin Zinevych <mitchsvik@gmail.com>
2016-05-02 23:53:23 +0300

    version 0.0.4.7
    +State and settings menu templates;

---
+++ src/com/coursework/frames/SettingsMenu.java
@@ -0,0 +1,16 @@
+package com.coursework.frames;
+
+import javax.swing.*;
+
+/**
+ * Created by Veniamin Zinevych on 02.05.2016.
+ */
+public class SettingsMenu extends JFrame {
+
+    SettingsMenu() {
+        setSize(360, 480);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+}
---
+++ src/com/coursework/frames/StateMenu.java
@@ -0,0 +1,15 @@
+package com.coursework.frames;
+
+import javax.swing.*;
+
+/**
+ * Created by Veniamin Zinevych on 02.05.2016.
+ */
+public class StateMenu extends JFrame {
+    StateMenu() {
+        setSize(360, 480);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+}
