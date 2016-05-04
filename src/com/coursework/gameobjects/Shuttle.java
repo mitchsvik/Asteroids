724cca03202d3d08299131c85bc631c449e84388
Veniamin Zinevych <mitchsvik@gmail.com>
2016-05-04 21:28:35 +0300

    version 0.0.5.9
    +Shuttle default specifications:

--- src/com/coursework/gameobjects/Shuttle.java
+++ src/com/coursework/gameobjects/Shuttle.java
@@ -1,9 +1,24 @@
 package com.coursework.gameobjects;
 
+import java.awt.image.BufferedImage;
+
 /**
  * Created by Veniamin Zinevych on 28.04.2016.
  */
 public class Shuttle implements GameObject {
+    private int radius;
+    private int shieldRadius;
+    private int life;
+    private int energy;
+    private BufferedImage texture;
+
+    public Shuttle() {
+        radius = 9;
+        shieldRadius = radius + 5;
+        life = 3;
+        energy = 100;
+    }
+
     @Override
     public boolean equals(Object obj) {
         if (obj instanceof Shuttle){
--- src/com/coursework/main/GameEngine.java
+++ src/com/coursework/main/GameEngine.java
@@ -1,6 +1,7 @@
 package com.coursework.main;
 
 import com.coursework.frames.Canvas;
+import com.coursework.gameobjects.Shuttle;
 
 /**
  * Created by Veniamin Zinevych on 03.05.2016.
@@ -8,10 +9,13 @@ import com.coursework.frames.Canvas;
 public class GameEngine {
     private Engine engine;
     private Canvas canvas;
+    private Shuttle shuttle;
 
     public GameEngine(Engine e) {
         engine = e;
         canvas = new Canvas("Asteroids", this);
 
+        shuttle = new Shuttle();
+        canvas.add(shuttle);
     }
 }
