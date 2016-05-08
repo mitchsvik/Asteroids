df5d0668de2cc337a44de556f67912cbaee72b7c
Veniamin Zinevych <mitchsvik@gmail.com>
2016-05-08 20:28:32 +0300

    version 0.0.9.13
    +2-dimensional vector realization;
    (parameters, constructors, get/set methods,  normalize, scale and
    getDistanceTo functions)

--- src/com/coursework/util/Vector2d.java
+++ src/com/coursework/util/Vector2d.java
@@ -1,7 +1,70 @@
 package com.coursework.util;
 
 /**
+ * 2-dimensional vector.
  * Created by Veniamin Zinevych on 07.05.2016.
+ * @author Veniamin Zinevych
  */
+
 public class Vector2d {
+    private double x;
+    private double y;
+
+    public Vector2d(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Vector2d(double angle) {
+        this.x = Math.cos(angle);
+        this.y = Math.sin(angle);
+    }
+
+    public Vector2d(Vector2d vector) {
+        this.x = vector.getX();
+        this.y = vector.getY();
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void set(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void scale(double scalar) {
+        x *= scalar;
+        y *= scalar;
+    }
+
+    public double getLength() {
+        return Math.sqrt(x * x + y * y);
+    }
+
+        double length = getLength();
+        if(length != 0.0 && length != 1.0) {
+            x /= length;
+            y /= length;
+        }
+    }
+
+    public double getDistanceTo(Vector2d vector) {
+        double dx = x - vector.getX();
+        double dy = y - vector.getY();
+        return Math.sqrt(dx * dx + dy * dy);
+    }
 }
